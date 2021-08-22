c3918d83dc35db792f88a5436479a8b9c0b658fc
Sol Lee <82362278+solleedata@users.noreply.github.com>
2021-08-22 12:23:19 +0900

    added derived table

--- SQL_grammar.sql
+++ SQL_grammar.sql
@@ -607,3 +607,16 @@ WHERE amount = (
         SELECT MAX(amount)
         FROM payments
     );
+--
+SELECT 
+    productCode, 
+    ROUND(SUM(quantityOrdered * priceEach)) sales
+FROM
+    orderdetails
+        INNER JOIN
+    orders USING (orderNumber)
+WHERE
+    YEAR(shippedDate) = 2003
+GROUP BY productCode
+ORDER BY sales DESC
+LIMIT 5;
